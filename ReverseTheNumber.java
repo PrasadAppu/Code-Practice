4b729e586b8e7ef2b678253765dbcb9497a7fffb
PrasadAppu <appuyadav094@gmail.com>
2020-06-30 22:58:32 +0530

    commit

---
+++ ReverseTheNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class ReverseTheNumber {
+    public static void main(String [] args){
+        Scanner sc =new Scanner(System.in);
+        int tasteCases = sc.nextInt();
+        while(tasteCases  --> 0){
+            int num =sc.nextInt();
+            int var ;
+            int res =0;
+            while(num!=0){
+                var =num%10;
+                res =res*10 +var;
+                num =num/10;
+            }
+            System.out.println(res);
+        }
+        sc.close();
+    }
+}
