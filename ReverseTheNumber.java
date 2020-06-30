73d85267ce0dd0e228c46b8f2e82fe6c6e8e6750
PrasadAppu <appuyadav094@gmail.com>
2020-06-30 23:09:48 +0530

    git git

--- ReverseTheNumber.java
+++ ReverseTheNumber.java
@@ -12,7 +12,7 @@ class ReverseTheNumber {
                 res =res*10 +var;
                 num =num/10;
             }
-            System.out.println(res);
+            System.out.println("reverse value is -" +res);
         }
         sc.close();
     }
